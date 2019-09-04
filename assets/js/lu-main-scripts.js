e25954812a7211b53e2e41b60a093e505ad0b1d5
luisurruela <luis.urruela@gmail.com>
2019-09-03 21:29:42 -0500

    finish testing

--- assets/js/lu-main-scripts.js
+++ assets/js/lu-main-scripts.js
@@ -1,13 +1,4 @@
 
 window.addEventListener('load', function() {
 
-    document.querySelector('#enviar').addEventListener('click', function(e) {
-        e.preventDefault();
-
-        let name = document.querySelector('#nombre').value;
-        let p = document.createElement('p');
-        p.innerText = name;
-        document.querySelector('section').appendChild(p);
-    });
-
 });
