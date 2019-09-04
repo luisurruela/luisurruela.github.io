937821e2fbd8be4a5a73323a958ae99705158055
Luis Urruela <luis.urruela@gmail.com>
2019-09-04 18:36:55 -0500

    testing with chef

--- assets/js/lu-main-scripts.js
+++ assets/js/lu-main-scripts.js
@@ -2,4 +2,31 @@
 
 window.addEventListener('load', function() {
 
+    const btn = document.getElementById('clickme');
+    btn.addEventListener('mouseover', () => {
+
+        $('#chef').fadeIn('800', function() {
+
+        });
+
+        $('#chef-img').rotate({
+            angle: 0,
+            animateTo:360
+        });
+        
+    });
+
+    $('#clickme').mouseout(function () { 
+        
+        $('#chef').fadeOut('500', function() {
+
+        });
+
+        $('#chef-img').rotate({
+            angle: 360,
+            animateTo: 0
+        });
+
+    });
+
 });
