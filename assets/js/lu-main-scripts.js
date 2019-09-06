46f8f22b970f42bdc63ef1abd1cf6ea4e6645319
Luis Urruela <luis.urruela@gmail.com>
2019-09-06 12:09:04 -0500

    delete chef testing files

--- assets/js/lu-main-scripts.js
+++ assets/js/lu-main-scripts.js
@@ -2,25 +2,6 @@
 
 window.addEventListener('load', function() {
 
-    $('#clickme').hover(function (e) {
 
-            $('#chef').fadeIn('800');
-            console.log(e);
-            $('#chef-img').rotate({
-                angle: 0,
-                animateTo: 360
-            });
-            
-        }, function () {
-            
-            $('#chef').fadeOut('500');
-
-            $('#chef-img').rotate({
-                angle: 360,
-                animateTo: 0
-            });
-
-        }
-    );
 
 });
