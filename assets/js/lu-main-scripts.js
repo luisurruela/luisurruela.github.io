7b8b65a9ab386b8ebdacc215b0e77d3b74e1e9e1
Luis Urruela <luis.urruela@gmail.com>
2019-09-06 13:22:37 -0500

    style

--- assets/js/lu-main-scripts.js
+++ assets/js/lu-main-scripts.js
@@ -1,15 +1,16 @@
 'use_strict';
 
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', function() {
 
-    
     $('.projects').slick({
         dots: true,
         infinite: true,
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        autoplay: true
+        autoplay: true,
+        adaptiveHeight: true,
+        autoplaySpeed: 2500
     });
 
 });
