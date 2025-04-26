a627109591ef1620cfa7cba75e9c3016df950043
luisurruela <luis.urruela@gmail.com>
2025-04-26 02:02:30 -0600

    Added tailwind and Astro Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    site: `https://luisurruela.github.io`,
+  site: `https://luisurruela.github.io`,
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
 });
