dd0f57a891520d37356d8607f09bb15a54c7bf97
luisurruela <luis.urruela@gmail.com>
2025-04-28 08:16:12 -0600

    Intalled react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: `https://luisurruela.github.io`,
@@ -13,5 +15,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [icon()],
+  integrations: [icon(), react()],
 });
