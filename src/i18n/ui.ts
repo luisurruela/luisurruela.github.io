19421234fc680301bde5216cbf17c1ff83725c42
luisurruela <luis.urruela@gmail.com>
2025-05-02 16:26:56 -0600

    Integrated i18n for multilanguage support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,35 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "nav.about": "Acerca de",
+    "nav.services": "Servicios",
+    "nav.contact": "Contacto",
+    "nav.menu": "Menú",
+    "nav.language": "Idioma",
+    "home.title": "Programador y Diseñador Web",
+    "home.subtitle": "Soy Luis Urruela",
+    "home.description":
+            "Tengo más de 25 años convirtiendo ideas en productos digitales, creativos y funcionales. Si buscas a alguien que te ayude a llevar tu proyecto o negocio al siguiente nivel ¡Envía mensaje!",
+    "home.primary-button": "Enviar mensaje",
+    "home.secondary-button": "Mis servicios",
+  },
+  en: {
+    "nav.about": "About",
+    "nav.services": "Services",
+    "nav.contact": "Contact",
+    "nav.menu": "Menu",
+    "nav.language": "Language",
+    "home.title": "Developer & Web Designer",
+    "home.subtitle": "I am Luis Urruela",
+    "home.description":
+          "I have over 25 years of experience turning ideas into digital, creative, and functional products. If you're looking for someone to help take your project or business to the next level, send me a message!",
+    "home.primary-button": "Send a message",
+    "home.secondary-button": "See my services",
+  },
+} as const;
